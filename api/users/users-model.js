7aa3d5ce2ad9a7c70b5db7c46ae5d8545c055a73
Austin Carman <abcarman12@gmail.com>
2021-06-22 12:41:43 -0600

    add new User wired. Needs to be fleshed out

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -5,7 +5,12 @@ function getUserById(user_id) {
     return (`hurray! you found user ${user_id}`)
 }
 
+function addUser(newUser) {
+    return (`new User ${newUser.username} has been added with password ${newUser.password}`)
+}
+
 
 module.exports = {
     getUserById,
+    addUser
 }
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { getUserById } = require('./users-model');
+const { getUserById, addUser } = require('./users-model');
 
 router.get('/', (req, res, next) => {
     res.json('congrats! Router wired')
@@ -8,6 +8,12 @@ router.get('/', (req, res, next) => {
 router.get('/:user_id', async (req, res, next) => {
     const result = await getUserById(req.params.user_id)
     res.json(result);
+})
+
+router.post('/', async (req, res, next) => {
+    const { username, password } = req.body
+    const newUser = await addUser(req.body)
+    res.json(newUser)
 })
 
 module.exports = router;
